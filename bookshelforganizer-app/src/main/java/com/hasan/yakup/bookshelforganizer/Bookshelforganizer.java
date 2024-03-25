48db9a8d677a5f791a4a9a0880d37b371df2249f
yakup <yakup_eroglu22@erdogan.edu.tr>
2024-03-26 02:09:28 +0300

    tryParseInt function edited

--- bookshelforganizer-app/src/main/java/com/hasan/yakup/bookshelforganizer/Bookshelforganizer.java
+++ bookshelforganizer-app/src/main/java/com/hasan/yakup/bookshelforganizer/Bookshelforganizer.java
@@ -1,49 +1,90 @@
-/**
-
-@file Bookshelforganizer.java
-@brief This file serves as a demonstration file for the Bookshelforganizer class.
-@details This file contains the implementation of the Bookshelforganizer class, which provides various mathematical operations.
-*/
-
-/**
-
-@package com.ucoruh.bookshelforganizer
-@brief The com.ucoruh.bookshelforganizer package contains all the classes and files related to the Bookshelforganizer App.
-*/
 package com.hasan.yakup.bookshelforganizer;
 
-import org.slf4j.LoggerFactory;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
-import ch.qos.logback.classic.Logger;
 /**
-
-@class Bookshelforganizer
-@brief This class represents a Bookshelforganizer that performs mathematical operations.
-@details The Bookshelforganizer class provides methods to perform mathematical operations such as addition, subtraction, multiplication, and division. It also supports logging functionality using the logger object.
-@author ugur.coruh
-*/
+ * @brief Bookshelforganizer class for managing bookshelf operations.
+ */
 public class Bookshelforganizer {
+    /** Scanner object for user input. */
+    public Scanner scanner;
+    /** PrintStream object for output. */
+    public PrintStream out;
+    /** Currently logged-in user. */
+    public User loggedUser;
+    /** Indicates whether the program is in test mode or not. */
+    public boolean isTestMode = false;
 
-  /**
-   * @brief Logger for the Bookshelforganizer class.
-   */
-  private static final Logger logger = (Logger) LoggerFactory.getLogger(Bookshelforganizer.class);
+    /**
+     * @brief Sets the logged-in user.
+     * @param user The user to be logged in.
+     * @return Always returns true to indicate successful user login.
+     */
+    public Boolean setLoggedUser(User user) {
+        this.loggedUser = user;
+        return true;
+    }
 
-  /**
-   * @brief Calculates the sum of two integers.
-   *
-   * @details This function takes two integer values, `a` and `b`, and returns their sum. It also logs a message using the logger object.
-   *
-   * @param a The first integer value.
-   * @param b The second integer value.
-   * @return The sum of `a` and `b`.
-   */
-  public int add(int a, int b) {
-    // Logging an informational message
-    logger.info("Logging message");
-    // Logging an error message
-    logger.error("Error message");
-    // Returning the sum of `a` and `b`
-    return a + b;
-  }
+    /**
+     * @brief Constructor for Bookshelforganizer.
+     * @param scanner Scanner object for user input.
+     * @param out     PrintStream object for output.
+     */
+    public Bookshelforganizer(Scanner scanner, PrintStream out) {
+        this.scanner = scanner;
+        this.out = out;
+    }
+
+    /**
+     * @brief Clears the console screen.
+     * @throws InterruptedException If the thread is interrupted while waiting.
+     * @throws IOException          If an I/O error occurs.
+     */
+    public void clearScreen() throws InterruptedException, IOException {
+        String operatingSystem = System.getProperty("os.name");
+        if (operatingSystem.contains("Windows")) {
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        } else {
+            out.print("\033[H\033[2J");
+            out.flush();
+        }
+    }
+
+    /**
+     * @brief Handles input errors by displaying a message to the user.
+     * @return Always returns false to indicate an input error.
+     */
+    public boolean handleInputError() {
+        out.println("Only enter numerical value");
+        return false;
+    }
+
+    /**
+     * @brief Attempts to parse the given string into an integer.
+     * @param value The string to be parsed.
+     * @return The parsed integer if successful, or -1 if parsing fails.
+     */
+    public int tryParseInt(String value) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+    }
 }
