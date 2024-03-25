1628ce35466c02f2041206deea58aeb045ffe58e
HBasriTaskin <taskin.hsnbasri@gmail.com>
2024-03-26 00:53:38 +0300

    App test writed

--- bookshelforganizer-app/src/test/java/com/hasan/yakup/bookshelforganizer/BookshelforganizerAppTest.java
+++ bookshelforganizer-app/src/test/java/com/hasan/yakup/bookshelforganizer/BookshelforganizerAppTest.java
@@ -10,8 +10,10 @@ import static org.junit.Assert.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.util.Scanner;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -19,117 +21,43 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.hasan.yakup.bookshelforganizer.BookshelforganizerApp;
-
-
 /**
-
-@class BookshelforganizerAppTest
-@brief This class represents the test class for the BookshelforganizerApp class.
-@details The BookshelforganizerAppTest class provides test methods to verify the behavior of the BookshelforganizerApp class. It includes test methods for successful execution, object creation, and error handling scenarios.
-@author ugur.coruh
-*/
+ * 
+ * @class BookshelforganizerAppTest
+ * @brief This class represents the test class for the BookshelforganizerApp
+ *        class.
+ * @details The BookshelforganizerAppTest class provides test methods to verify
+ *          the behavior of the BookshelforganizerApp class. It includes test
+ *          methods for successful execution, object creation, and error
+ *          handling scenarios.
+ * @author ugur.coruh
+ */
 public class BookshelforganizerAppTest {
+  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+  private final PrintStream originalOut = System.out;
+  private final ByteArrayInputStream inContent = new ByteArrayInputStream("3".getBytes());
+  BookshelforganizerApp librarysystemApp = new BookshelforganizerApp();
 
-  /**
-   * @brief This method is executed once before all test methods.
-   * @throws Exception
-   */
-  @BeforeClass
-  public static void setUpBeforeClass() throws Exception {
-  }
-
-  /**
-   * @brief This method is executed once after all test methods.
-   * @throws Exception
-   */
-  @AfterClass
-  public static void tearDownAfterClass() throws Exception {
-  }
-
-  /**
-   * @brief This method is executed before each test method.
-   * @throws Exception
-   */
   @Before
-  public void setUp() throws Exception {
+  public void setUpStreams() {
+    System.setOut(new PrintStream(outContent));
+    System.setIn(inContent);
   }
 
-  /**
-   * @brief This method is executed after each test method.
-   * @throws Exception
-   */
   @After
-  public void tearDown() throws Exception {
-  }
-
-  /**
-   * @brief Test method to validate the successful execution of the main method.
-   *
-   * @details This method redirects the System.in and System.out streams to simulate user input and capture the output. It calls the main method of BookshelforganizerApp with a valid argument and asserts the expected behavior based on the output.
-   */
-  @Test
-  public void testMainSuccess() {
-    // Redirect System.in and System.out
-    InputStream originalIn = System.in;
-    PrintStream originalOut = System.out;
-    // Create a ByteArrayInputStream with the desired input
-    String input = System.lineSeparator(); // Pressing "Enter" key
-    ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
-    // Redirect System.in to the ByteArrayInputStream
-    System.setIn(inputStream);
-    // Create a ByteArrayOutputStream to capture the output
-    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    System.setOut(new PrintStream(outputStream));
-    String[] args = new String[] {"0"};
-    // Call the main method of BookshelforganizerApp
-    BookshelforganizerApp.main(args);
-    // Restore original System.in and System.out
-    System.setIn(originalIn);
+  public void restoreStreams() {
     System.setOut(originalOut);
-    // Assert the desired behavior based on the output
-    assertTrue(true);
+    System.setIn(System.in);
   }
 
-  /**
-   * @brief Test method to validate the object creation of BookshelforganizerApp.
-   *
-   * @details This method creates an instance of the BookshelforganizerApp class and asserts the successful creation of the object.
-   */
   @Test
-  public void testMainObject() {
-    // Creating an instance of BookshelforganizerApp
-    BookshelforganizerApp app = new BookshelforganizerApp();
-    // Asserting the successful creation of the object
-    assertTrue(true);
-  }
-
-  /**
-   * @brief Test method to validate the error handling of the main method.
-   *
-   * @details This method redirects the System.in and System.out streams to simulate user input and capture the output. It calls the main method of BookshelforganizerApp with an invalid argument and asserts the expected behavior based on the output.
-   */
-  @Test
-  public void testMainError() {
-    // Redirect System.in and System.out
-    InputStream originalIn = System.in;
-    PrintStream originalOut = System.out;
-    // Create a ByteArrayInputStream with the desired input
-    String input = System.lineSeparator(); // Pressing "Enter" key
-    ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
-    // Redirect System.in to the ByteArrayInputStream
-    System.setIn(inputStream);
-    // Create a ByteArrayOutputStream to capture the output
-    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    System.setOut(new PrintStream(outputStream));
-    String[] args = new String[] {"1"};
-    // Call the main method of BookshelforganizerApp
+  public void testLibrarysystemAppMain() throws IOException, InterruptedException, ClassNotFoundException {
+    String[] args = null;
     BookshelforganizerApp.main(args);
-    // Restore original System.in and System.out
-    System.setIn(originalIn);
-    System.setOut(originalOut);
-    // Assert the desired behavior based on the output
-    assertTrue(true);
-  }
 
+    String expectedOutputStartsWith = "Welcome To Virtual Bookshelf Organizer";
+    String actualOutput = outContent.toString();
+
+    assertEquals(true, actualOutput.startsWith(expectedOutputStartsWith));
+  }
 }
