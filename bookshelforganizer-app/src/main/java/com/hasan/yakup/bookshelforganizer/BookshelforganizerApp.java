546ddd61fa57f9a0e32ab1736bfc56342d18c240
yakup <yakup_eroglu22@erdogan.edu.tr>
2024-03-26 02:07:26 +0300

    App.java added

--- bookshelforganizer-app/src/main/java/com/hasan/yakup/bookshelforganizer/BookshelforganizerApp.java
+++ bookshelforganizer-app/src/main/java/com/hasan/yakup/bookshelforganizer/BookshelforganizerApp.java
@@ -1,80 +1,41 @@
-/**
-
-@file BookshelforganizerApp.java
-@brief This file serves as the main application file for the Bookshelforganizer App.
-@details This file contains the entry point of the application, which is the main method. It initializes the necessary components and executes the Bookshelforganizer App.
-*/
-/**
-
-@package com.ucoruh.bookshelforganizer
-@brief The com.ucoruh.bookshelforganizer package contains all the classes and files related to the Bookshelforganizer App.
-*/
 package com.hasan.yakup.bookshelforganizer;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
-
-import org.slf4j.LoggerFactory;
-
-import ch.qos.logback.classic.Logger;
+import java.util.Scanner;
 
 /**
- *
- * @class BookshelforganizerApp
- * @brief This class represents the main application class for the Bookshelforganizer
- *        App.
- * @details The BookshelforganizerApp class provides the entry point for the Bookshelforganizer
- *          App. It initializes the necessary components, performs calculations,
- *          and handles exceptions.
- * @author ugur.coruh
+ * The main class for the Bookshelf Organizer application.
+ * This class initializes the application, creates necessary file paths,
+ * and invokes the main menu of the Bookshelf Organizer system.
+ * 
+ * @author Hasan And Yakup
+ * @version 1.0
+ * @since 2024-03-21
  */
 public class BookshelforganizerApp {
-  /**
-   * @brief Logger for the BookshelforganizerApp class.
-   */
-  private static final Logger logger = (Logger) LoggerFactory.getLogger(BookshelforganizerApp.class);
-
-  /**
-   * @brief The main entry point of the Bookshelforganizer App.
-   *
-   * @details The main method is the starting point of the Bookshelforganizer App. It
-   *          initializes the logger, performs logging, displays a greeting
-   *          message, and handles user input.
-   *
-   * @param args The command-line arguments passed to the application.
-   */
-  public static void main(String[] args) {
-    // Logging messages for informational purposes
-    logger.info("Logging message");
-    // Logging an error message
-    logger.error("Error message");
-    // Displaying a greeting message
-    System.out.println("Hello World!");
-
-    try {
-      // Checking if command-line arguments are provided
-      if (args != null) {
-        // Checking if there are any arguments
-        if (args.length > 0) {
-          // Checking if the first argument is "1"
-          if (args[0].equals("1")) {
-            // Throwing a dummy IOException
-            throw new IOException("Dummy Exception...");
-          }
-        }
-      }
-
-      // Prompting the user to press Enter to continue
-      System.out.println("Press Enter to Continue...");
-      // Reading user input from the console
-      System.in.read();
-      // Displaying a closing message
-      System.out.println("Thank you...");
-    } catch (IOException e) {
-      // Logging the exception
-      logger.error(e.toString());
-      // Printing the exception stack trace
-      e.printStackTrace();
+    /**
+     * The main method of the application.
+     * It initializes the necessary components, including scanner and bookshelf
+     * organizer,
+     * specifies file paths, and invokes the main menu of the Bookshelf Organizer
+     * system.
+     * 
+     * @param args Command-line arguments (not used in this application).
+     * @throws FileNotFoundException  If the specified file is not found.
+     * @throws IOException            If an I/O error occurs.
+     * @throws InterruptedException   If a thread is interrupted.
+     * @throws ClassNotFoundException If a class is not found during
+     *                                deserialization.
+     */
+    public static void main(String[] args)
+            throws FileNotFoundException, IOException, InterruptedException, ClassNotFoundException {
+        Scanner inputScanner = new Scanner(System.in);
+        Bookshelforganizer bookshelforganizer = new Bookshelforganizer(inputScanner, System.out);
+        String pathFileBooks = "books.bin";
+        String pathFileUsers = "users.bin";
+        String pathFileWishlist = "wishlists.bin";
+        String pathFileLendingHistories = "lendinghistories.bin";
+        bookshelforganizer.mainMenu(pathFileUsers, pathFileBooks, pathFileLendingHistories, pathFileWishlist);
     }
-  }
-
 }
