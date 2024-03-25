a5473e3c63328a22980eccf08d73de170f5651bc
yakup <yakup_eroglu22@erdogan.edu.tr>
2024-03-26 02:11:34 +0300

    Heap Sort swapBooks functions added

--- bookshelforganizer-app/src/main/java/com/hasan/yakup/bookshelforganizer/Bookshelforganizer.java
+++ bookshelforganizer-app/src/main/java/com/hasan/yakup/bookshelforganizer/Bookshelforganizer.java
@@ -418,6 +418,153 @@ public class Bookshelforganizer {
 
         return books.get(books.size() - 1).getId() + 1;
     }
+    
+    /**
+     * @brief Gets the ID for a new wishlist.
+     * @param pathFileWishlist The file path to the wishlist data.
+     * @return The ID for a new wishlist.
+     * @throws FileNotFoundException  If the specified file is not found.
+     * @throws IOException            If an I/O error occurs.
+     * @throws ClassNotFoundException If the class of a serialized object cannot be
+     *                                found.
+     */
+    public int getNewWishlistId(String pathFileWishlist)
+            throws FileNotFoundException, IOException, ClassNotFoundException {
+        List<Book> wishlist = loadWishlist(pathFileWishlist);
+        if (wishlist.isEmpty())
+            return 1;
 
+        randomizedQuickSortBookIds(wishlist, 0, wishlist.size() - 1);
+
+        return wishlist.get(wishlist.size() - 1).getId() + 1;
+    }
+
+    /**
+     * @brief Sorts the book IDs using randomized quicksort algorithm.
+     * @param books The list of books to be sorted.
+     * @param low   The index of the lower bound.
+     * @param high  The index of the upper bound.
+     */
+    public void randomizedQuickSortBookIds(List<Book> books, int low, int high) {
+        if (low < high) {
+            int pivotIndex = randomizedPartitionBookIds(books, low, high);
+            randomizedQuickSortBookIds(books, low, pivotIndex); // Hoare Partitioning'de pivotIndex dahil edilir.
+            randomizedQuickSortBookIds(books, pivotIndex + 1, high);
+        }
+    }
+
+    /**
+     * @brief Partitions the book IDs randomly.
+     * @param books The list of books to be partitioned.
+     * @param low   The index of the lower bound.
+     * @param high  The index of the upper bound.
+     * @return The index of the pivot element.
+     */
+    public int randomizedPartitionBookIds(List<Book> books, int low, int high) {
+        int pivotIndex = low + (int) (Math.random() * (high - low + 1));
+        Book pivot = books.get(pivotIndex);
+        int i = low - 1;
+        int j = high + 1;
+        while (true) {
+            do {
+                i++;
+            } while (books.get(i).getId() < pivot.getId());
+            do {
+                j--;
+            } while (books.get(j).getId() > pivot.getId());
+            if (i >= j) {
+                return j;
+            }
+            swapBooks(books, i, j);
+        }
+    }
+
+    /**
+     * @brief Swaps two books in the list.
+     * @param books The list of books.
+     * @param i     The index of the first book.
+     * @param j     The index of the second book.
+     */
+    public void swapBooks(List<Book> books, int i, int j) {
+        Book temp = books.get(i);
+        books.set(i, books.get(j));
+        books.set(j, temp);
+    }
+
+    /**
+     * @brief Gets the ID for a new user.
+     * @param pathFileUsers The file path to the user data.
+     * @return The ID for a new user.
+     * @throws FileNotFoundException  If the specified file is not found.
+     * @throws IOException            If an I/O error occurs.
+     * @throws ClassNotFoundException If the class of a serialized object cannot be
+     *                                found.
+     */
+    public int getNewUserId(String pathFileUsers) throws FileNotFoundException, IOException, ClassNotFoundException {
+        // Load the list of users from the given file path.
+        List<User> users = loadUsers(pathFileUsers);
+
+        // Convert user IDs into an array for sorting
+        int[] userIds = new int[users.size()];
+        for (int i = 0; i < users.size(); i++) {
+            userIds[i] = users.get(i).getId();
+        }
+
+        // Use HeapSort to sort the array
+        heapSort(userIds);
+
+        // If the array is empty, return 1 as the new user ID
+        if (userIds.length == 0) {
+            return 1;
+        }
+
+        // Otherwise, return the highest user ID plus one
+        return userIds[userIds.length - 1] + 1;
+    }
+
+    /**
+     * @brief Sorts the array using the heap sort algorithm.
+     * @param arr The array to be sorted.
+     */
+    // HeapSort implementation
+    public void heapSort(int[] arr) {
+        int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+        for (int i = n - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr, i, 0);
+        }
+    }
+
+    /**
+     * @brief Heapifies the array.
+     * @param arr The array to be heapified.
+     * @param n   The size of the heap.
+     * @param i   The index of the root node.
+     * @return Always returns true.
+     */
+    public boolean heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest])
+            largest = left;
+
+        if (right < n && arr[right] > arr[largest])
+            largest = right;
+
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            heapify(arr, n, largest);
+        }
+        return true;
+    }
 
 }
